224daf1f5d3e611cef86d224d396c956f41aa792
EggPool <dev@eggpool.net>
2019-09-27 21:16:39 +0200

    Voting, index page

--- wallet/crystals/020_bismuthvote/__init__.py
+++ wallet/crystals/020_bismuthvote/__init__.py
@@ -5,8 +5,10 @@ Bismuth Voting Crystal for Tornado wallet
 from os import path, listdir
 
 from modules.basehandlers import CrystalHandler
-from modules.helpers import base_path
+from modules.helpers import base_path, get_private_dir
 from tornado.template import Template
+from bismuthvoting.bip39 import BIP39
+from secrets import token_bytes
 
 DEFAULT_THEME_PATH = path.join(base_path(), "crystals/020_bismuthvote/themes/default")
 
@@ -15,9 +17,75 @@ MODULES = {}
 __version__ = "0.1"
 
 
+MASTER_KEY = ""
+KEYFILE = ""
+
+# Temporarily hardcoded
+# material status is the icon name assignment_late: ongoing assignment_turned_in: ended, assignment: not started yet
+BGVP_MOTIONS = [
+    {
+        "Motion_txid": "motion_0_txid_this_would_be_a_b64_encoded_string",
+        "Motion_number": "TEST",
+        "Motion_url": "https://hypernodes.bismuth.live/?p=863",
+        "Motion address": "FAKE_ADDRESS_DO_NOT_USE",
+        "Vote_start_date": 1569931200,
+        "Vote_reading_date": 1572609600,
+        "Vote_end_date": 1573387200,
+        "Options": [
+            {"option_value": "A", "option_title": "Test motion vote A"},
+            {"option_value": "B", "option_title": "Test motion vote B"},
+        ],
+        "material_status": "assignment_late",
+        "text_status": "TEST only"
+    },
+    {
+        "Motion_txid": "motion_1_txid_this_would_be_a_b64_encoded_string",
+        "Motion_number": 1,
+        "Motion_url": "https://hypernodes.bismuth.live/?p=820",
+        "Motion address": "FAKE_ADDRESS_DO_NOT_USE",
+        "Vote_start_date": 1569931200,
+        "Vote_reading_date": 1572609600,
+        "Vote_end_date": 1573387200,
+        "Options": [
+            {"option_value": "A", "option_title": "Do not change supply emission."},
+            {
+                "option_value": "B",
+                "option_title": "Change the supply emission from block 1,450,000 to lower the dilution.",
+            },
+        ],
+        "material_status": "assignment",
+        "text_status": "Not started yet"
+    },
+]
+
+
 class BismuthvoteHandler(CrystalHandler):
     async def about(self, params=None):
-        self.render("about.html", bismuth=self.bismuth_vars, version=__version__)
+        voting = {
+            "masterkey": MASTER_KEY,
+            "masterkey_file": KEYFILE,
+            "key_check": BIP39.check(MASTER_KEY),
+        }
+        # TODO: get from the chain (+ cache)
+        voting["bgvp_motions"] = BGVP_MOTIONS
+        self.render(
+            "about.html", bismuth=self.bismuth_vars, version=__version__, voting=voting
+        )
+
+    async def set_key(self, params=None):
+        masterkey = self.get_argument("masterkey", None)
+        # print("key", masterkey)
+        if not masterkey:
+            # Generate a new one
+            entropy = token_bytes(16)
+            print(entropy)
+            bip39 = BIP39(entropy)
+            masterkey = bip39.to_mnemonic()
+        with open(KEYFILE, "w") as fp:
+            fp.write(masterkey)
+        global MASTER_KEY
+        MASTER_KEY = masterkey
+        self.redirect("/crystal/bismuthvote/")
 
     async def get(self, command=""):
         command, *params = command.split("/")
@@ -39,8 +107,14 @@ def action_init(params=None):
         module_name = module.split(".")[0]
         file_name = path.join(modules_dir, module)
         with open(file_name, "rb") as f:
-            MODULES[module_name] = Template(f.read().decode("utf-8"))
+            MODULES[module_name] = Template(f.read().decode("utf-8"))            
     """
+    global MASTER_KEY
+    global KEYFILE
+    KEYFILE = path.join(get_private_dir(), "votingkey.json")
+    if path.isfile(KEYFILE):
+        with open(KEYFILE) as fp:
+            MASTER_KEY = fp.readline()
 
 
 def filter_home(params):
