c72f1d9c0b70aa8b9e32eab3661a37aa23106898
HCLivess <admin@bismuth.cz>
2020-03-05 21:52:33 +0100

    - favicon filtered though icofx to ensure compatibility - added cleanup to make_dist to prevent interference with older builds - nuitka compilation script - innosetup installer script

--- wallet/make_dist_nuitka.bat
+++ wallet/make_dist_nuitka.bat
@@ -4,12 +4,14 @@ del /f /s /q build 1>nul
 rmdir /s /q build
 
 copy wallet.py TornadoBismuthWallet.py
-pyinstaller --hidden-import tornado.locale --hidden-import aiohttp --onefile --icon=favicon.ico TornadoBismuthWallet.py
+robocopy . TornadoBismuthWallet.build favicon.ico
+python -m nuitka --follow-imports TornadoBismuthWallet.py --standalone --show-progress -j 8 --recurse-all --windows-icon=favicon.ico
+ren TornadoBismuthWallet.dist dist
+ren TornadoBismuthWallet.build build
 robocopy locale dist/locale /S /E *.mo
 mkdir dist/themes
 robocopy themes/material dist/themes/material /S /E
 robocopy crystals dist/crystals /S /E
-
 
 REM see https://nsis.sourceforge.io/Main_Page , make installer from zip
 REM Or inno setup https://cyrille.rossant.net/create-a-standalone-windows-installer-for-your-python-application/
