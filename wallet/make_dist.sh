c3931bf8a51a46ef4843616b393334d453584445
geho2 <geho2@bismuth.live>
2020-05-22 07:11:17 +0200

    Instructions for Linux build

--- wallet/make_dist.sh
+++ wallet/make_dist.sh
@@ -7,3 +7,20 @@ cp -r themes/material dist/themes/material
 cp -r themes/common dist/themes/common
 cp -r crystals dist/crystals
 rm TornadoBismuthWallet.py
+
+# Compilation of TornadoWallet on Ubuntu 16.04
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+# curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+# export PATH="~/.pyenv/bin:$PATH"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+# env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.7
+# pyenv local 3.7.7
+# git clone https://github.com/Bismuthfoundation/TornadoWallet.git
+# cd TornadoWallet
+# pip install -r requirements.txt
+# pip install pyinstaller
+# cd wallet
+# ./make_dist.sh
