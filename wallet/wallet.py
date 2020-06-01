8a7ae87bac633e9ed781f9992d17f6c8aadadc8e
geho2 <geho2@bismuth.live>
2020-06-01 18:15:08 +0200

    Bump version

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -39,7 +39,7 @@ from modules import helpers
 from modules.crystals import CrystalManager
 from modules import i18n  # helps pyinstaller, do not remove
 
-__version__ = "0.1.36"
+__version__ = "0.1.37"
 
 define("port", default=8888, help="run on the given port", type=int)
 define(
